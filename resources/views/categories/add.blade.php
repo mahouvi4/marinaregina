377558d50daf8393581fe06f4b0d642790c15848
mahouvi4 <93478652+mahouvi4@users.noreply.github.com>
2022-05-15 19:23:11 +0100

    je viens de creer dossier categorie

---
+++ resources/views/categories/add.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    https://github.com/mahouvi4/marinaregina.git
+    git push -u origin main
+</head>
+<body>
+<h1>DIEU FERA</h1>
+</body>
+</html>
+
